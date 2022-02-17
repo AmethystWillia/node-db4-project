c387adbb135cb17a4c5f93ee5c3960b3bbed7735
Amethyst Willia <zmithic@gmail.com>
2022-02-17 16:45:52 -0600

    Set up knexfile and made first integration

---
+++ data/migrations/20220217223436_create-tables.js
@@ -0,0 +1,39 @@
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('recipes', tbl => {
+        tbl.increments('recipe_id');
+        tbl.string('recipe_name').notNullable().unique();
+    })
+    .createTable('steps', tbl => {
+        tbl.increments('step_id');
+        tbl.integer().notNullable();
+        tbl.string('instructions').notNullable();
+        tbl.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id').inTable('recipes');
+    })
+    .createTable('ingredients', tbl => {
+        tbl.increments('ingredient_id');
+        tbl.string('ingredient_name', 120).notNullable();
+        tbl.integer('step_id')
+            .unsigned()
+            .notNullable()
+            .references('step_id').inTable('steps');
+    })
+    .createTable('steps_ingredients', tbl => {
+        tbl.primary(['step_id', 'ingredient_id']);
+        tbl.integer('step_id')
+            .unsigned()
+            .notNullable()
+            .references('step_id').inTable('steps');
+        tbl.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('ingredient_id').inTable('ingredients');
+        tbl.string('ingredient_quantity').notNullable();
+    });
+};
+
+exports.down = function(knex) {
+};
