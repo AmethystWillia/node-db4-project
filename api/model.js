7abe7c646fb6d622429430afe0442833e26e3a47
Amethyst Willia <zmithic@gmail.com>
2022-02-17 18:22:54 -0600

    I fporgot to switch branches oops

--- api/model.js
+++ api/model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config');
 
 async function getRecipeById(id) {
     const result = await db('recipes as r')
-        .leftJoin()
+        .leftJoin('steps as s', 's.recipe_id', 'r.recipe_id')
+        .
 }
 
 module.exports = { getRecipeById };
--- api/router.js
+++ api/router.js
@@ -3,4 +3,16 @@ const Recipe = require('./model');
 
 const router = express.Router();
 
+router.get('/id', (req, res, next) => {
+    const { id } = req.params;
+
+    Recipe.getRecipeById(id)
+        .then(recipe => {
+            res.json(recipe);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
+
 module.exports = router;
