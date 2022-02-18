cc981c8c2f2a505ba9862622d7774641fa17abfe
Amethyst Willia <zmithic@gmail.com>
2022-02-17 18:33:41 -0600

    Ignore last commit, I got confused. Can't figure out the getByRecipeId but completed all other tasks

--- api/model.js
+++ api/model.js
@@ -1,10 +1,10 @@
 const db = require('../data/db-config');
 
-async function getRecipeById(id) {
+async function getRecipeById(recipe_id) {
     const result = await db('recipes as r')
         .leftJoin('steps as s', 's.recipe_id', 'r.recipe_id')
         .select('r.recipe_id', 'r.recipe_name', 's.step_number', 's.step_instructions')
-        .where('r.recipe_id', id);
+        .where('r.recipe_id', recipe_id);
 
     return result;
 }
